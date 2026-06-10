ef4b3fbe42d2ea06552c54883824090aa025b146
Syeda-Rabia49 <ms.rabia016@gmail.com>
2026-06-10 20:49:32 +0500

    secure api key

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
 
-client = Groq(api_key="YOUR_API_KEY_HERE")
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 with open("knowledge.txt", "r") as f:
     knowledge = f.read()
@@ -17,7 +21,7 @@ def chat():
     response = client.chat.completions.create(
         model="llama-3.3-70b-versatile",
         messages=[
-           {"role": "system", "content": "You are a helpful assistant for Oakwood Essentials. Use this info: " + knowledge + " Rules: Keep answers short. Use clean bullet points on new lines. If the user makes a spelling mistake try to understand what they meant and answer anyway. Max 3 sentences or 4 bullet points."},
+            {"role": "system", "content": "You are a helpful assistant for Oakwood Essentials. Use this info: " + knowledge + " Rules: Keep answers short. Use clean bullet points on new lines. If the user makes a spelling mistake try to understand what they meant and answer anyway. Max 3 sentences or 4 bullet points."},
             {"role": "user", "content": user_message}
         ],
         max_tokens=200
